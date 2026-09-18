714d4a8190ce8318c9458d9fb185cea9422bdb91
giulio d'erme <gde00555@gmail.com>
2026-09-18 12:47:27 +0200

    fix AML compiler NUL typing import

--- recall_aml/service.py
+++ recall_aml/service.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 from datetime import datetime, timezone
 import logging
 import time
+from typing import Any
 
 from recall.types import Chunk
 from recall_aml.compiler import Compiler, QueryPlan, deterministic_extract
